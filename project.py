1227e50ded7fafe5992ac6d702b159979d7d3abd
farzad <farzad.ghlm8@gmail.com>
2023-01-21 17:34:26 -0600

    add persons

--- project.py
+++ project.py
@@ -1,14 +1,38 @@
 # manage a school
-class Student:
-    def __init__(self, name, grade):
-        self.name = name
-        self.grade = grade
 
-class Teacher:
-    def __init__(self, name, subject):
-        self.name = name
-        self.subject = subject
+class Person:
+    def __init__(self,fname,lname,bdate,natcode):
+        self.fname=fname
+        self.lname = lname
+        self.bdate= bdate
+        self.natcode = natcode
+    def full_name(self,fname,lname):
+        return f'name is : {fname} lname is {lname}'
+    def show_brithday(self,bdate):
+        return f'brith date is {bdate}'
+    def show_natcode(self,natcode):
+        return f'national code is {natcode}'
 
+class Student(Person):
+    def __init__(self, major):
+        self.major = major
+    def show_major(self,major):
+        return f'major is {major}'
+
+class Teacher(Person):
+    def __init__(self, teacher_code,hire_t):
+        self.teacher_code = teacher_code
+        self.hire = hire
+    def show_teach_code(self,teacher_code):
+        return f'teacher code is {teacher_code}'
+
+    def show_hire_time(self,hire_t):
+        return f'hire time is {hire_t}'
+
+
+
+
+######################################## sudo code
 class School:
     def __init__(self, name, students, teachers):
         self.name = name
