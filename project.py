6a6b31f07b76723a1dede53117f7a481e2b37553
farzad <farzad.ghlm8@gmail.com>
2023-01-21 17:18:20 -0600

    add classes

--- project.py
+++ project.py
@@ -1,2 +1,29 @@
-print('hello world')
-print('user.username')
+# manage a school
+class Student:
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+
+class Teacher:
+    def __init__(self, name, subject):
+        self.name = name
+        self.subject = subject
+
+class School:
+    def __init__(self, name, students, teachers):
+        self.name = name
+        self.students = students
+        self.teachers = teachers
+
+    def get_student_names(self):
+        return [s.name for s in self.students]
+
+    def get_teacher_names(self):
+        return [t.name for t in self.teachers]
+
+students = [Student("Alice", "9th"), Student("Bob", "10th")]
+teachers = [Teacher("Charlie", "Math"), Teacher("Dave", "Science")]
+school = School("My School", students, teachers)
+
+print("Student names:", school.get_student_names())
+print("Teacher names:", school.get_teacher_names())
