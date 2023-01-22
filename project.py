71ab44078d5d957b561904b4ac9e8c6c3b9315e6
farzad <farzad.ghlm8@gmail.com>
2023-01-21 18:16:05 -0600

    add info

--- project.py
+++ project.py
@@ -1,53 +1,74 @@
-# manage a school
-
-class Person:
-    def __init__(self,fname,lname,bdate,natcode):
-        self.fname=fname
-        self.lname = lname
-        self.bdate= bdate
-        self.natcode = natcode
-    def full_name(self,fname,lname):
-        return f'name is : {fname} lname is {lname}'
-    def show_brithday(self,bdate):
-        return f'brith date is {bdate}'
-    def show_natcode(self,natcode):
-        return f'national code is {natcode}'
-
-class Student(Person):
-    def __init__(self, major):
-        self.major = major
-    def show_major(self,major):
-        return f'major is {major}'
-
-class Teacher(Person):
-    def __init__(self, teacher_code,hire_t):
-        self.teacher_code = teacher_code
-        self.hire = hire
-    def show_teach_code(self,teacher_code):
-        return f'teacher code is {teacher_code}'
-
-    def show_hire_time(self,hire_t):
-        return f'hire time is {hire_t}'
-
-
-
-
-######################################## sudo code
 class School:
-    def __init__(self, name, students, teachers):
+    def __init__(self, name, code):
         self.name = name
-        self.students = students
-        self.teachers = teachers
+        self.code = code
+        self.classes = {}
+        
+    def add_class(self, class_name, class_code):
+        self.classes[class_code] = Class(class_name, class_code)
+        
+    def get_class(self, class_code):
+        return self.classes[class_code]
+    
+    def show_all_info(self):
+        print("School Name: ", self.name)
+        print("School Code: ", self.code)
+        print("Classes: ")
+        for class_code in self.classes:
+            class_info = self.classes[class_code]
+            print("   Class Name: ", class_info.name)
+            print("   Class Code: ", class_info.code)
+            print("   Students: ")
+            for student in class_info.students:
+                print("      Name: ", student.name)
+                print("      Birthdate: ", student.birthdate)
+                print("      ID: ", student.id)
+                print("      Major: ", student.major)
+            print("   Teachers: ")
+            for teacher in class_info.teachers:
+                print("      Name: ", teacher.name)
+                print("      ID: ", teacher.id)
+                print("      Birthdate: ", teacher.birthdate)
+                print("      Teacher Code: ", teacher.teacher_code)
+                print("      Hire Date: ", teacher.hire_date)
 
-    def get_student_names(self):
-        return [s.name for s in self.students]
+class Class:
+    def __init__(self, name, code):
+        self.name = name
+        self.code = code
+        self.students = []
+        self.teachers = []
+        
+    def add_student(self, name, birthdate, id, major):
+        self.students.append(Student(name, birthdate, id, major))
+        
+    def add_teacher(self, name, id, birthdate, teacher_code, hire_date):
+        self.teachers.append(Teacher(name, id, birthdate, teacher_code, hire_date))
 
-    def get_teacher_names(self):
-        return [t.name for t in self.teachers]
+class Student:
+    def __init__(self, name, birthdate, id, major):
+        self.name = name
+        self.birthdate = birthdate
+        self.id = id
+        self.major = major
 
-students = [Student("Alice", "9th"), Student("Bob", "10th")]
-teachers = [Teacher("Charlie", "Math"), Teacher("Dave", "Science")]
-school = School("My School", students, teachers)
+class Teacher:
+    def __init__(self, name, id, birthdate, teacher_code, hire_date):
+        self.name = name
+        self.id = id
+        self.birthdate = birthdate
+        self.teacher_code = teacher_code
+        self.hire_date = hire_date
+        
+# Create an instance of School
+high_school = School("Example High School", "EHS123")
 
-print("Student names:", school.get_student_names())
-print("Teacher names:", school.get_teacher_names())
+# Get number of classes from user
+num_classes = int(input("Enter the number of classes in the high school: "))
+
+# Get information for each class
+for i in range(num_classes):
+    class_name = input("Enter the name of class: ")
+    class_code = input("Enter the code of class: ")
+    high_school.add_class(class_name, class_code)
+    
